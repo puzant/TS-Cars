2b04ab42785366e5bd3ad3b421056875a64729cb
Puzant <PBAKJEJIAN@hotmail.com>
2019-05-31 12:14:58 +0300

    added new tests
    - added sinon library to test HTTP request
    - added tests for Fetcher Class

--- src/CarMechanic.ts
+++ src/CarMechanic.ts
@@ -5,6 +5,7 @@ export class CarMechanic {
     /**
      * the mechanic is responsible of building the car
      * @param meterial, the metrial needed to build the car 
+     * this method is generic it'll build the same type of car
      */
      static build(meterial) {
        return CarFactory.createCar(meterial)
--- tests/unit/Fetcher.spec.ts
+++ tests/unit/Fetcher.spec.ts
@@ -1,13 +1,30 @@
+import request from 'request'
 import { expect } from 'chai'
 import 'mocha'
-//  mock data to be served from this URL: https://api.myjson.com/bins/18kemw
+//  mock data to be served from this URL: https://api.myjson.com/bins/1eg3bc
+
+//  import sinon
+import sinon from 'sinon'
 
 //  import Fetcher
 import { Fetcher } from '../../src/services/Fetcher';
 
 
 describe ('Fetcher', () => {
+    before(() => {
+        sinon
+        .stub(request, 'get')
+        .yields(null, null, {data: 200})
+    })
+
     it ('should do get request', () => {
-       
+       Fetcher.get('https://api.myjson.com/bins/1eg3bc').then((res) => {
+       console.log('Get request was successful')
+       expect(res.model).to.equal("Ferrari 458 Italia")
+       expect(res.color).to.equal('red');
+       })
+       .catch((err) => {
+           throw err;
+       })
     })
 })
